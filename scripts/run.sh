0520b8bd97ca3f7c837eb47a17a0ced5c0bb3b09
nixnodes <code@nixnodes.net>
2015-05-31 21:54:52 +0200

    Signed-off-by: nixnodes <code@nixnodes.net>

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,8 @@ BASE_PATH=`dirname ${0}`
 
 rm -f ${BASE_PATH}/build/tier1/* 
 
+${BASE_PATH}/build_tier1_arpa.sh 10.0.0.0_8 1
 ${BASE_PATH}/build_tier1_arpa.sh 172.20.0.0_16 1
 ${BASE_PATH}/build_tier1_arpa.sh 172.22.0.0_16 1
 ${BASE_PATH}/build_tier1_arpa.sh 172.23.0.0_16 1
+${BASE_PATH}/build_tier1_arpa.sh 172.31.0.0_16 1
