67cdd769065b228f0be5be6dee0390178646868d
nixnodes <code@nixnodes.net>
2015-06-02 23:11:49 +0200

    Signed-off-by: nixnodes <code@nixnodes.net>

--- scripts/build_tier1.sh
+++ scripts/build_tier1.sh
@@ -19,8 +19,15 @@ mkdir -p ${OUT_PATH}/tier1
 generate_soa ${SERVER_NAME_TIER1} "" > ${OUT_PATH}/tier1/root.db
 generate_forward_zone ${REGISTRY_PATH}/dns/root-servers.dn42 ""  >> ${OUT_PATH}/tier1/root.db
 
+generate_soa ${SERVER_NAME_TIER1} root-servers.dn42 > ${OUT_PATH}/tier1/root-servers.dn42.db
+generate_forward_zone ${REGISTRY_PATH}/dns/root-servers.dn42 root-servers.dn42 >> ${OUT_PATH}/tier1/root-servers.dn42.db
+
 generate_soa ${SERVER_NAME_TIER1} dn42 > ${OUT_PATH}/tier1/${1}.db 
 generate_forward_zone ${REGISTRY_PATH}/dns/zone-servers.dn42 dn42 >> ${OUT_PATH}/tier1/${1}.db 
+
+generate_soa ${SERVER_NAME_TIER1} arpa > ${OUT_PATH}/tier1/arpa.db
+generate_forward_zone ${REGISTRY_PATH}/dns/root-servers.dn42 arpa noglue >> ${OUT_PATH}/tier1/arpa.db
+generate_forward_zone ${REGISTRY_PATH}/dns/arpa in-addr.arpa >> ${OUT_PATH}/tier1/arpa.db
 
 for file in ${REGISTRY_PATH}/dns/*.${1}; do
 	[[ "${TIER1_RESTRICT_ZONES}" = *${1}* ]] && continue
@@ -32,6 +39,7 @@ generate_soa ${SERVER_NAME_TIER1} zone-servers.dn42 > ${OUT_PATH}/tier1/zone-ser
 generate_forward_zone ${REGISTRY_PATH}/dns/zone-servers.dn42 zone-servers.dn42  >>  ${OUT_PATH}/tier1/zone-servers.dn42.db 
 
 cu_add_master_zone ${OUT_PATH}/tier1/named.conf "." ${OUT_PATH}/tier1/root.db
+cu_add_master_zone ${OUT_PATH}/tier1/named.conf root-servers.dn42 ${OUT_PATH}/tier1/root-servers.dn42.db
 cu_add_master_zone ${OUT_PATH}/tier1/named.conf zone-servers.dn42 ${OUT_PATH}/tier1/zone-servers.dn42.db 
 cu_add_master_zone ${OUT_PATH}/tier1/named.conf ${1} ${OUT_PATH}/tier1/${1}.db 
 
