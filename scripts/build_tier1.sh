2fa1d8b1a7a9f7df6ba02d94863f67395fc083c6
nixnodes <code@nixnodes.net>
2015-06-02 23:33:55 +0200

    Signed-off-by: nixnodes <code@nixnodes.net>

--- scripts/build_tier1.sh
+++ scripts/build_tier1.sh
@@ -39,6 +39,7 @@ generate_soa ${SERVER_NAME_TIER1} zone-servers.dn42 > ${OUT_PATH}/tier1/zone-ser
 generate_forward_zone ${REGISTRY_PATH}/dns/zone-servers.dn42 zone-servers.dn42  >>  ${OUT_PATH}/tier1/zone-servers.dn42.db 
 
 cu_add_master_zone ${OUT_PATH}/tier1/named.conf "." ${OUT_PATH}/tier1/root.db
+cu_add_master_zone ${OUT_PATH}/tier1/named.conf "arpa" ${OUT_PATH}/tier0/arpa.db
 cu_add_master_zone ${OUT_PATH}/tier1/named.conf root-servers.dn42 ${OUT_PATH}/tier1/root-servers.dn42.db
 cu_add_master_zone ${OUT_PATH}/tier1/named.conf zone-servers.dn42 ${OUT_PATH}/tier1/zone-servers.dn42.db 
 cu_add_master_zone ${OUT_PATH}/tier1/named.conf ${1} ${OUT_PATH}/tier1/${1}.db 
