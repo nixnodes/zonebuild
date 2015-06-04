20b321cef8c6010a2f97cf437f9d36f196aa84ee
nixnodes <code@nixnodes.net>
2015-06-04 06:12:08 +0200

    fix tier1 forward zone gen filter
    Signed-off-by: nixnodes <code@nixnodes.net>

--- scripts/build_tier1.sh
+++ scripts/build_tier1.sh
@@ -28,9 +28,13 @@ generate_forward_zone ${REGISTRY_PATH}/dns/zone-servers.dn42 dn42 >> ${OUT_PATH}
 generate_soa ${SERVER_NAME_TIER1} in-addr-servers.arpa > ${OUT_PATH}/tier1/in-addr-servers.arpa.db
 generate_forward_zone ${REGISTRY_PATH}/dns/arpa in-addr-servers.arpa >> ${OUT_PATH}/tier1/in-addr-servers.arpa.db
 
+#generate_soa ${SERVER_NAME_TIER1} in-addr.arpa > ${OUT_PATH}/tier1/in-addr.arpa.db
+#generate_forward_zone ${REGISTRY_PATH}/dns/arpa in-addr.arpa noglue >> ${OUT_PATH}/tier1/in-addr.arpa.db
+
 for file in ${REGISTRY_PATH}/dns/*.${1}; do
-	[[ "${TIER1_RESTRICT_ZONES}" = *${1}* ]] && continue
 	zone=`basename $file`
+	[[ "${TIER1_FORWARD_ZONES_RESTRICT[@]}" = *${zone}* ]] && continue
+	echo ${zone}
 	generate_forward_zone ${file} ${zone} >> ${OUT_PATH}/tier1/${1}.db 
 done
 
@@ -38,7 +42,7 @@ generate_soa ${SERVER_NAME_TIER1} zone-servers.dn42 > ${OUT_PATH}/tier1/zone-ser
 generate_forward_zone ${REGISTRY_PATH}/dns/zone-servers.dn42 zone-servers.dn42  >>  ${OUT_PATH}/tier1/zone-servers.dn42.db 
 
 cu_add_master_zone ${OUT_PATH}/tier1/named.conf "." ${OUT_PATH}/tier1/root.db
-#cu_add_master_zone ${OUT_PATH}/tier1/named.conf "arpa" ${OUT_PATH}/tier0/arpa.db
+#cu_add_master_zone ${OUT_PATH}/tier1/named.conf in-addr.arpa ${OUT_PATH}/tier1/in-addr.arpa.db
 cu_add_master_zone ${OUT_PATH}/tier1/named.conf in-addr-servers.arpa ${OUT_PATH}/tier1/in-addr-servers.arpa.db
 cu_add_master_zone ${OUT_PATH}/tier1/named.conf root-servers.dn42 ${OUT_PATH}/tier1/root-servers.dn42.db
 cu_add_master_zone ${OUT_PATH}/tier1/named.conf zone-servers.dn42 ${OUT_PATH}/tier1/zone-servers.dn42.db 
