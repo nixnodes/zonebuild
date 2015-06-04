9d775ab82f2f013a1dcef8cfd2aef995b4549473
nixnodes <code@nixnodes.net>
2015-06-05 01:52:30 +0200

    add ipv6 zones to named.conf
    Signed-off-by: nixnodes <code@nixnodes.net>

--- scripts/build_tier0.sh
+++ scripts/build_tier0.sh
@@ -109,6 +109,7 @@ b_path="${b_path}"')$'
 		generate_soa ${SERVER_NAME_TIER0} ${zone}.ip6.arpa > ${OUT_PATH}/tier0/db.${zone}.ip6.arpa
 		generate_forward_zone ${REGISTRY_PATH}/dns/root-servers.dn42 ${zone}.ip6.arpa noglue >> ${OUT_PATH}/tier0/db.${zone}.ip6.arpa
 		generate_forward_zone ${REGISTRY_PATH}/dns/in-addr-servers.dn42 ${zone}.ip6.arpa >> ${OUT_PATH}/tier0/db.${zone}.ip6.arpa
+		cu_add_master_zone ${OUT_PATH}/tier0/named.conf ${zone}.ip6.arpa ${OUT_PATH}/tier0/db.${zone}.ip6.arpa
 	done
 }
 
