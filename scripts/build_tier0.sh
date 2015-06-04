f1dcebb391338f9cdb3e18a20f1cb0305d8a77a9
nixnodes <code@nixnodes.net>
2015-06-04 23:12:51 +0200

    Signed-off-by: nixnodes <code@nixnodes.net>

--- scripts/build_tier0.sh
+++ scripts/build_tier0.sh
@@ -40,11 +40,11 @@ generate_forward_zone ${REGISTRY_PATH}/dns/dn42-servers.dn42 dn42-servers.dn42  
 		
 get_icann_root_zone() {
 	for item in ${ICANN_AXFR_ENABLED_ROOTS[@]}; do
-		axfr_zone . ${item} | egrep '^[^.;]' && break
+		axfr_zone . ${item} | egrep "${1}" && break
 	done
 }
 
-[ ${MERGE_ICANN_ROOT} -gt 0 ] && get_icann_root_zone | egrep -v '^arpa.' >> ${OUT_PATH}/tier0/root.db
+[ ${MERGE_ICANN_ROOT} -gt 0 ] && get_icann_root_zone '^([^.;]|${MERGE_RESTRICT_ZONES}.)' >> ${OUT_PATH}/tier0/root.db
 
 cu_add_master_zone ${OUT_PATH}/tier0/named.conf "." ${OUT_PATH}/tier0/root.db
 cu_add_master_zone ${OUT_PATH}/tier0/named.conf "in-addr-servers.dn42" ${OUT_PATH}/tier0/in-addr-servers.dn42.db
@@ -56,6 +56,9 @@ cu_add_master_zone ${OUT_PATH}/tier0/named.conf "root.dn42" ${OUT_PATH}/tier0/ro
 
 mkdir -p ${OUT_PATH}/tmp
 rm -f ${OUT_PATH}/tmp/*.bt0.tmp
+
+b_path='^('
+OCTETS=()
 
 for item in ${ARPA_ZONES[@]}; do	
 	ZNAME=(`${ZBUILD} -build inetnum --path ${REGISTRY_PATH}/inetnum --root ${item} \
@@ -73,11 +76,34 @@ for item in ${ARPA_ZONES[@]}; do
 		generate_soa ${SERVER_NAME_TIER0} ${o_octet}.in-addr.arpa >> ${OUT_PATH}/tier0/${o_octet}.in-addr.arpa.db
 		generate_forward_zone ${REGISTRY_PATH}/dns/root-servers.dn42 ${o_octet}.in-addr.arpa noglue >> ${OUT_PATH}/tier0/${o_octet}.in-addr.arpa.db
 		cu_add_master_zone ${OUT_PATH}/tier0/named.conf ${o_octet}.in-addr.arpa ${OUT_PATH}/tier0/${o_octet}.in-addr.arpa.db
+		OCTETS=(${OCTETS[@]} ${o_octet})
 		touch ${OUT_PATH}/tmp/az-${o_octet}.bt0.tmp
 	} 
 	
 	generate_forward_zone ${REGISTRY_PATH}/dns/in-addr-servers.dn42 ${ZNAME}.in-addr.arpa noglue >> ${OUT_PATH}/tier0/${o_octet}.in-addr.arpa.db
 	
+	b_path="${b_path}${ZNAME}|"
+	
 done
+
+b_path=`echo ${b_path} | sed -r 's/\|$//'`
+b_path="${b_path}"')$'
+
+icann_root=`get_icann_root_zone | egrep '^\.' | \
+		egrep 'root-servers.net' | egrep 'IN.*NS'`
+
+
+for octet in ${OCTETS[@]}; do
+	[ ${octet} = 10 ] && continue
+	i=0
+	while [ ${i} -lt 256 ]; do
+		echo ${i}.${octet} | egrep -q "${b_path}" || {
+			echo "${icann_root}" | sed -r "s/^./${i}.${octet}.in-addr.arpa./" >> ${OUT_PATH}/tier0/${octet}.in-addr.arpa.db
+		}
+		i=$[i+1]
+	done
+done
+
+
 
 exit 0
