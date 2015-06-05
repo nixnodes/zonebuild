3c9064db817fb33de7b77d7d07355025510e0ed8
nixnodes <code@nixnodes.net>
2015-06-05 04:16:54 +0200

    Signed-off-by: nixnodes <code@nixnodes.net>

--- scripts/build_tier1.sh
+++ scripts/build_tier1.sh
@@ -22,8 +22,13 @@ generate_forward_zone ${REGISTRY_PATH}/dns/root-servers.dn42 ""  >> ${OUT_PATH}/
 generate_soa ${SERVER_NAME_TIER1} root-servers.dn42 > ${OUT_PATH}/tier1/root-servers.dn42.db
 generate_forward_zone ${REGISTRY_PATH}/dns/root-servers.dn42 root-servers.dn42 >> ${OUT_PATH}/tier1/root-servers.dn42.db
 
-generate_soa ${SERVER_NAME_TIER1} dn42 > ${OUT_PATH}/tier1/${1}.db 
-generate_forward_zone ${REGISTRY_PATH}/dns/zone-servers.dn42 dn42 >> ${OUT_PATH}/tier1/${1}.db 
+generate_soa ${SERVER_NAME_TIER1} ${1} > ${OUT_PATH}/tier1/${1}.db 
+
+if [[ "${1}" != "dn42" ]]; then
+	generate_forward_zone ${REGISTRY_PATH}/dns/${1} ${1} >> ${OUT_PATH}/tier1/${1}.db 
+else
+	generate_forward_zone ${REGISTRY_PATH}/dns/zone-servers.dn42 ${1} >> ${OUT_PATH}/tier1/${1}.db 
+fi
 
 generate_soa ${SERVER_NAME_TIER1} in-addr-servers.dn42 > ${OUT_PATH}/tier1/in-addr-servers.dn42.db
 generate_forward_zone ${REGISTRY_PATH}/dns/in-addr-servers.dn42 in-addr-servers.dn42 >> ${OUT_PATH}/tier1/in-addr-servers.dn42.db
@@ -49,5 +54,5 @@ cu_add_master_zone ${OUT_PATH}/tier1/named.conf "." ${OUT_PATH}/tier1/root.db
 cu_add_master_zone ${OUT_PATH}/tier1/named.conf in-addr-servers.dn42 ${OUT_PATH}/tier1/in-addr-servers.dn42.db
 cu_add_master_zone ${OUT_PATH}/tier1/named.conf root-servers.dn42 ${OUT_PATH}/tier1/root-servers.dn42.db
 cu_add_master_zone ${OUT_PATH}/tier1/named.conf zone-servers.dn42 ${OUT_PATH}/tier1/zone-servers.dn42.db 
-cu_add_master_zone ${OUT_PATH}/tier0/named.conf dn42-servers.dn42 ${OUT_PATH}/tier1/dn42-servers.dn42.db
+cu_add_master_zone ${OUT_PATH}/tier1/named.conf dn42-servers.dn42 ${OUT_PATH}/tier1/dn42-servers.dn42.db
 cu_add_master_zone ${OUT_PATH}/tier1/named.conf ${1} ${OUT_PATH}/tier1/${1}.db 
