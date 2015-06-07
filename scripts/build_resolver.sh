31667e9ccc2335abb6169012cbd5f6ef0ce330ad
nixnodes <code@nixnodes.net>
2015-06-07 07:42:37 +0200

    Signed-off-by: nixnodes <code@nixnodes.net>

--- scripts/build_resolver.sh
+++ scripts/build_resolver.sh
@@ -10,6 +10,7 @@ rm -f ${OUT_PATH}/res/*
 mkdir -p ${OUT_PATH}/res
 
 cu_add_hint_zone ${OUT_PATH}/res/named.conf '.' ${OUT_PATH}/res/hints.db
+
 generate_forward_zone ${REGISTRY_PATH}/dns/root-servers.dn42 "" "" 3600000 >> ${OUT_PATH}/res/hints.db
 
 root_ips=(`cat ${OUT_PATH}/res/hints.db | egrep 'IN A' | sort -u | cut -d ' ' -f 5`)
@@ -36,20 +37,22 @@ for zone in ${TIER1_ZONES[@]}; do
 	cu_add_forwarders_zone ${OUT_PATH}/res/named-forwards.conf ${zone} "${forwarders}"
 done
 
-exit 0
+#exit 0
+
+cu_add_forwarders_zone ${OUT_PATH}/res/named-forwards.conf '.' "${forwarders}"
 
 echo "${0}: [R]: generating RFC1918 zones"
 
 i=16
 while [ ${i} -lt 32 ]; do
-	generate_soa a.resolvers.dn42 ${i}.172.in-addr.arpa > ${OUT_PATH}/res/${i}.172.in-addr.arpa.db
-	generate_forward_zone ${REGISTRY_PATH}/dns/root-servers.dn42 ${i}.172.in-addr.arpa noglue >> ${OUT_PATH}/res/${i}.172.in-addr.arpa.db
-	cu_add_master_zone ${OUT_PATH}/res/named.conf  ${i}.172.in-addr.arpa ${OUT_PATH}/res/${i}.172.in-addr.arpa.db
+	#generate_soa a.resolvers.dn42 ${i}.172.in-addr.arpa > ${OUT_PATH}/res/${i}.172.in-addr.arpa.db
+	#generate_forward_zone ${REGISTRY_PATH}/dns/root-servers.dn42 ${i}.172.in-addr.arpa noglue >> ${OUT_PATH}/res/${i}.172.in-addr.arpa.db
+	cu_add_forwarders_zone ${OUT_PATH}/res/named.conf ${i}.172.in-addr.arpa "${forwarders}"
 	i=$[i+1]
 done
 
-generate_soa a.resolvers.dn42 10.in-addr.arpa > ${OUT_PATH}/res/10.in-addr.arpa.db
-generate_forward_zone ${REGISTRY_PATH}/dns/root-servers.dn42 10.in-addr.arpa noglue >> ${OUT_PATH}/res/10.in-addr.arpa.db
-cu_add_master_zone ${OUT_PATH}/res/named.conf 10.in-addr.arpa ${OUT_PATH}/res/10.in-addr.arpa.db
+#generate_soa a.resolvers.dn42 10.in-addr.arpa > ${OUT_PATH}/res/10.in-addr.arpa.db
+#generate_forward_zone ${REGISTRY_PATH}/dns/root-servers.dn42 10.in-addr.arpa noglue >> ${OUT_PATH}/res/10.in-addr.arpa.db
+cu_add_forwarders_zone ${OUT_PATH}/res/named.conf 10.in-addr.arpa "${forwarders}"
 
 
