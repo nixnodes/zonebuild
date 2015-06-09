d138758f3d93887d1a2c6e982ff2332e4df7f5b7
nixnodes <code@nixnodes.net>
2015-06-09 09:58:47 +0200

    Signed-off-by: nixnodes <code@nixnodes.net>

--- scripts/build_tier1.sh
+++ scripts/build_tier1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #@VERSION:0
-#@REVISION:47
+#@REVISION:48
 
 if [ -n "${2}" ]; then
 	ucfile="${2}"
@@ -27,8 +27,8 @@ mkdir -p ${OUT_PATH}/tier1
 generate_soa ${SERVER_NAME_TIER1} "" > ${OUT_PATH}/tier1/root.db
 generate_forward_zone ${REGISTRY_PATH}/dns/root-servers.dn42 ""  >> ${OUT_PATH}/tier1/root.db
 
-#generate_soa ${SERVER_NAME_TIER1} root-servers.dn42 > ${OUT_PATH}/tier1/root-servers.dn42.db
-#generate_forward_zone ${REGISTRY_PATH}/dns/root-servers.dn42 root-servers.dn42 >> ${OUT_PATH}/tier1/root-servers.dn42.db
+generate_soa ${SERVER_NAME_TIER1} root-servers.dn42 > ${OUT_PATH}/tier1/root-servers.dn42.db
+generate_forward_zone ${REGISTRY_PATH}/dns/root-servers.dn42 root-servers.dn42 >> ${OUT_PATH}/tier1/root-servers.dn42.db
 
 generate_soa ${SERVER_NAME_TIER1} ${1} > ${OUT_PATH}/tier1/${1}.db 
 
@@ -44,8 +44,8 @@ generate_forward_zone ${REGISTRY_PATH}/dns/in-addr-servers.dn42 in-addr-servers.
 #generate_soa ${SERVER_NAME_TIER1} in-addr.arpa > ${OUT_PATH}/tier1/in-addr.arpa.db
 #generate_forward_zone ${REGISTRY_PATH}/dns/arpa in-addr.arpa noglue >> ${OUT_PATH}/tier1/in-addr.arpa.db
 
-#generate_soa ${SERVER_NAME_TIER1} dn42-servers.dn42 > ${OUT_PATH}/tier1/dn42-servers.dn42.db 
-#generate_forward_zone ${REGISTRY_PATH}/dns/dn42-servers.dn42 dn42-servers.dn42  >>  ${OUT_PATH}/tier1/dn42-servers.dn42.db 
+generate_soa ${SERVER_NAME_TIER1} dn42-servers.dn42 > ${OUT_PATH}/tier1/dn42-servers.dn42.db 
+generate_forward_zone ${REGISTRY_PATH}/dns/dn42-servers.dn42 dn42-servers.dn42  >>  ${OUT_PATH}/tier1/dn42-servers.dn42.db 
 
 for file in ${REGISTRY_PATH}/dns/*.${1}; do
 	[ -f "${file}" ] || continue
@@ -61,9 +61,9 @@ generate_forward_zone ${REGISTRY_PATH}/dns/zone-servers.dn42 zone-servers.dn42  
 cu_add_master_zone ${OUT_PATH}/tier1/named.conf "." ${OUT_PATH}/tier1/root.db
 #cu_add_master_zone ${OUT_PATH}/tier1/named.conf in-addr.arpa ${OUT_PATH}/tier1/in-addr.arpa.db
 cu_add_master_zone ${OUT_PATH}/tier1/named.conf in-addr-servers.dn42 ${OUT_PATH}/tier1/in-addr-servers.dn42.db
-#cu_add_master_zone ${OUT_PATH}/tier1/named.conf root-servers.dn42 ${OUT_PATH}/tier1/root-servers.dn42.db
+cu_add_master_zone ${OUT_PATH}/tier1/named.conf root-servers.dn42 ${OUT_PATH}/tier1/root-servers.dn42.db
 cu_add_master_zone ${OUT_PATH}/tier1/named.conf zone-servers.dn42 ${OUT_PATH}/tier1/zone-servers.dn42.db 
-#cu_add_master_zone ${OUT_PATH}/tier1/named.conf dn42-servers.dn42 ${OUT_PATH}/tier1/dn42-servers.dn42.db
+cu_add_master_zone ${OUT_PATH}/tier1/named.conf dn42-servers.dn42 ${OUT_PATH}/tier1/dn42-servers.dn42.db
 cu_add_master_zone ${OUT_PATH}/tier1/named.conf ${1} ${OUT_PATH}/tier1/${1}.db 
 
 exit 0
